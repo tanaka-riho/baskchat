199691ce806a64ffda271658ed47f0acf73a7376
tanari <tanari.info@gmail.com>
2020-05-20 01:03:44 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,4 +1,4 @@
-<header class="sm-4">
+<header class="md-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
         <a class="navbar-brand" href="/">Tokyo2020</a>
          
@@ -9,9 +9,22 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-outline-light btn-sm']) !!}
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                
+             @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-outline-light btn-sm']) !!}
+                    {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-outline-light btn-sm']) !!}
+                @endif
             </ul>
         </div>
     </nav>
-</header>
+</header>    
